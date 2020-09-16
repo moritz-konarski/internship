28718a6a368e0829c0d2cd2e878cba3fa58f0859
Moritz Konarski <moritz.konarski@gmail.com>
2020-09-16 13:09:22 +0600

    plotter: implement plotting for surface pressure and phis

--- programs/netcdf_plotter/main.py
+++ programs/netcdf_plotter/main.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 # This is a sample Python script.
-# inspired by https://joehamman.com/2013/10/12/plotting-netCDF-data-with-Python/
-# https://www2.atmos.umd.edu/~cmartin/python/examples/netcdf_example1.html
+# https://disc.gsfc.nasa.gov/information/howto?title=How%20to%20read%20and%20plot%20NetCDF%20MERRA-2%20data%20in%20Python
 
 from netCDF4 import Dataset
 import numpy as np
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
-#import fire
+# import fire
+
+import warnings
 
 # this is needed because netcdf4 emits warnings that I cannot fix when printing
 # data
-import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
+# errors from mismatched libraries
+warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 my_example_nc_file = '../.downloads/20200701.nc4'
-destination_file = 'nc_plot.png'
 file_format = 'png'
-file_dpi = 400
+file_dpi = 500
 
 
 def read_var_info() -> {str: (str, str)}:
@@ -42,10 +43,12 @@ def list():
     print_var_info(read_var_info())
 
 
-def create_graph(var: str):
+def create_graph(var: str, name: str):
     with Dataset(my_example_nc_file, mode='r') as data:
         lons: [float] = data.variables['lon'][:]
         lats: [float] = data.variables['lat'][:]
+
+        min, max = find_range(var)
 
         d = data.variables[var][:, :, :]
         d: [float, float, float] = d[0, :, :]
@@ -57,18 +60,39 @@ def create_graph(var: str):
         ax.coastlines(resolution="110m", linewidth=1)
         ax.gridlines(linestyle='--', color='black')
 
+        res = int((max - min) / 20)
+
         # Set contour levels, then draw the plot and a colorbar
-        clevs = np.arange(75000, 103366, 500)
-        plt.contourf(lons_1, lats_1, T_1, clevs, transform=ccrs.PlateCarree(),
+        clevs = np.arange(min, max, res)
+        plt.contourf(lons, lats, d, clevs, transform=ccrs.PlateCarree(),
                      cmap=plt.cm.jet)
-        plt.title('Surface Pressure at 12am 01.07.2020', size=14)
+
         cb = plt.colorbar(ax=ax, orientation="vertical", pad=0.02, aspect=16,
                           shrink=0.8)
-        cb.set_label('Pa', size=12, rotation=0, labelpad=15)
         cb.ax.tick_params(labelsize=10)
+        if name == 'ps':
+            plt.title('Surface Pressure at 12am 01.07.2020', size=14)
+            cb.set_label('Pa', size=12, rotation=0, labelpad=15)
+        elif name == 'phis':
+            plt.title('Surface Geopotential Height at 12am 01.07.2020', size=14)
+            cb.set_label('m^2/s^-2', size=12, rotation=0, labelpad=15)
 
         # Save the plot as a PNG image
-        fig.savefig(destination_file, format=file_format, dpi=file_dpi)
+        fig.savefig('nc_plot_' + name + '.png', format=file_format,
+                    dpi=file_dpi)
+
+
+def list_all_var_data():
+    with Dataset(my_example_nc_file, mode='r') as data:
+        print(data.variables)
+
+
+def find_range(var_name):
+    with Dataset(my_example_nc_file, mode='r') as data:
+        var = data.variables[var_name]
+        max = var[:].max()
+        min = var[:].min()
+    return min, max
 
 
 # TODO: use numpy variables for speed and accuracy -- read the type from input
@@ -77,28 +101,7 @@ def create_graph(var: str):
 if __name__ == '__main__':
     with Dataset(my_example_nc_file, mode='r') as cdata:
         list()
-        exit(0)
-        lons: [float] = data.variables['lon'][:]
-        lats: [float] = data.variables['lat'][:]
-        T = data.variables['PS'][:, :, :]
-        T: [float, float, float] = T[0, :, :]
-
-        # Set the figure size, projection, and extent
-        fig = plt.figure(figsize=(10, 6))
-        ax = plt.axes(projection=ccrs.Robinson())
-        ax.set_global()
-        ax.coastlines(resolution="110m", linewidth=1)
-        ax.gridlines(linestyle='--', color='black')
-
-        # Set contour levels, then draw the plot and a colorbar
-        clevs = np.arange(75000, 103366, 500)
-        plt.contourf(lons_1, lats_1, T_1, clevs, transform=ccrs.PlateCarree(),
-                     cmap=plt.cm.jet)
-        plt.title('Surface Pressure at 12am 01.07.2020', size=14)
-        cb = plt.colorbar(ax=ax, orientation="vertical", pad=0.02, aspect=16,
-                          shrink=0.8)
-        cb.set_label('Pa', size=12, rotation=0, labelpad=15)
-        cb.ax.tick_params(labelsize=10)
-
-        # Save the plot as a PNG image
-        fig.savefig(destination_file, format=file_format, dpi=file_dpi)
+        list_all_var_data()
+        # print(find_range('PS'))
+        create_graph('PS', 'ps')
+        create_graph('PHIS', 'phis')
