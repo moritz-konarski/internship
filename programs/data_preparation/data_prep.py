2c01a382fe8a77389fe15cce632fb29d0cf5aa0f
Moritz Konarski <moritz.konarski@gmail.com>
2020-10-07 14:47:16 +0600

    fix extractor, create simple time series and heatmap plotters

--- programs/data_preparation/data_prep.py
+++ programs/data_preparation/data_prep.py
@@ -9,6 +9,150 @@ from pathlib import Path
 from netCDF4 import Dataset
 
 
+def extract_var(src_path: str, dest_path: str, var_name: str):
+    if not re.findall(r"/$", dest_path):
+        dest_path + "/"
+    if not os.path.exists(dest_path):
+        os.mkdir(dest_path)
+
+    with open(dest_path + var_name + ".npz", 'wb') as f:
+        data = None
+        for part in sorted(Path(src_path).glob("*.nc4")):
+            filepath = os.path.join(part)
+            print(filepath)
+            with Dataset(filepath, 'r') as d:
+                if data is None:
+                    data = np.asarray(d.variables[var_name])
+                    print(data.shape)
+                else:
+                    data = np.append(data, np.asarray(d.variables[var_name]), 
+                            axis=0)
+                    print(data.shape)
+            np.savez_compressed(f, data, allow_pickle=True)
+
+def extract_time_lat_lon(src_path: str, dest_path: str):
+    if not re.findall(r"/$", dest_path):
+        dest_path + "/"
+    if not os.path.exists(dest_path):
+        os.mkdir(dest_path)
+
+    with open(dest_path + "time.npz", 'wb') as f:
+        part = sorted(Path(src_path).glob("*.nc4"))[0]
+        filepath = os.path.join(part)
+        print(filepath)
+        with Dataset(filepath, 'r') as d:
+            data = np.asarray(d.variables['time'])
+            print(data.shape)
+        np.savez_compressed(f, data, allow_pickle=True)
+
+    with open(dest_path + "lat.npz", 'wb') as f:
+        part = sorted(Path(src_path).glob("*.nc4"))[0]
+        filepath = os.path.join(part)
+        print(filepath)
+        with Dataset(filepath, 'r') as d:
+            data = np.asarray(d.variables['lat'])
+            print(data.shape)
+        np.savez_compressed(f, data, allow_pickle=True)
+
+    with open(dest_path + "lon.npz", 'wb') as f:
+        part = sorted(Path(src_path).glob("*.nc4"))[0]
+        filepath = os.path.join(part)
+        print(filepath)
+        with Dataset(filepath, 'r') as d:
+            data = np.asarray(d.variables['lon'])
+            print(data.shape)
+        np.savez_compressed(f, data, allow_pickle=True)
+
+    #with Dataset(path, 'r') as d:
+    #    for var_name in var_list:
+    #        var = np.asarray(d.variables[var_name])
+    #        file_name = os.path.realpath(store_path) + '/' + date + "_"     \
+    #                + var_name + ".npz"
+    #        with open(file_name, 'wb') as f:
+    #            np.savez_compressed(f, var, allow_pickle=True)
+
+    #with Dataset(path, 'r') as d:
+    #    var = np.array(d.variables[var_name])
+    #    np.save(os.path.realpath(store_path) + '/' + var_name + '.npy', var,
+    #            allow_pickle=True)
+
+def test_file(src_path: str):
+    src = np.load(src_path, allow_pickle=True)
+    print(src['arr_0'].shape)
+
+
+# TODO: make sure that only the files that have dates are used here
+# it should not try to load itself in the process
+def concat(name: str, path: str, dest: str):
+    with open(dest, 'wb') as f:
+        for part in Path(path).glob("*" + name + ".npz"):
+            filepath = os.path.join(part)
+            print(filepath)
+            dataArray = np.load(filepath, allow_pickle=True)
+            #print(dataArray['arr_0'])
+            np.savez_compressed(f, dataArray['arr_0'], allow_pickle=True)
+
+
+def extract_all(path: str, store_path: str):
+    var_list = get_var_name_list(path)
+    if not re.findall(r"/$", store_path):
+        store_path + "/"
+    if not os.path.exists(store_path):
+        os.mkdir(store_path)
+    date = get_date_range(path)
+    with Ds(path, 'r') as d:
+        for var_name in var_list:
+            var = np.asarray(d.variables[var_name])
+            file_name = os.path.realpath(store_path) + '/' + date + "_"     \
+                    + var_name + ".npz"
+            with open(file_name, 'wb') as f:
+                np.savez_compressed(f, var, allow_pickle=True)
+
+
+def create_metadata_file(data: str, dest: str, var_list: str):
+    pass
+
+
+def get_var_info_list(path: str) -> [str, str, str]:
+    """
+    List all variables, their names, uiniets in the specified file.
+    """
+    list = []
+    with Dataset(path, 'r') as d:
+        for var in d.variables.keys():
+            list.append((var, d.variables[var].long_name, 
+                    d.variables[var].units))#, d.variables[var].dimension))
+        print(d.variables)
+    return list
+
+
+def print_all_vars(path: str):
+    for (var, name, unit) in get_var_info_list(path):
+        print("{0:6s} {1:15s} {2:s}".format(var, unit, name))
+
+
+def get_date_range(path: str) -> str:
+    with Ds(path, 'r') as d:
+        return str(d.RangeBeginningDate) + "_" + str(d.RangeEndingDate)
+
+
+if __name__ == '__main__':
+    fire.Fire({
+        "list": print_all_vars,
+        "extract": extract_var,
+        "meta": extract_time_lat_lon,
+        "test": test_file
+    })
+
+    #concat('_PS', './test/', './test/PS.npz')
+    #concat('_H', './test/', './test/H.npz')
+    #concat('_O3', './test/', './test/O3.npz')
+    #concat('_EPV', './test/', './test/EPV.npz')
+    #check_src_path('../var_extract')
+    #check_dest_path('lol')
+    #check_dest_path('hi')
+
+
 def convert_path(str_path: str) -> Path:
     """
     Takes a string path as input and returns a Path object
@@ -53,72 +197,6 @@ def var_exists(path: Path, var_name: str) -> bool:
     Checks if the selected file contains the desired variable
     """
 
-def extract_var(str_src_path: str, str_dest_path: str, var_name: str):
-    """
-    Extracts one variable from the specified file and saves it 
-    """
-    src_path = check_src_path(str_src_path)
-    dest_path = check_dest_path(str_dest_path)
-    
-    # TODO: come up with a good variable name check in this setting
-    if var_exists(var_name):
-        pass
-
-    if not re.findall(r"/$", store_path):
-        store_path + "/"
-    if not os.path.exists(store_path):
-        os.mkdir(store_path)
-    with Ds(path, 'r') as d:
-        var = np.array(d.variables[var_name])
-        np.save(os.path.realpath(store_path) + '/' + var_name + '.npy', var,
-                allow_pickle=True)
-
-
-# TODO: make sure that only the files that have dates are used here
-# it should not try to load itself in the process
-def concat(name: str, path: str, dest: str):
-    with open(dest, 'wb') as f:
-        for part in Path(path).glob("*" + name + ".npz"):
-            filepath = os.path.join(part)
-            print(filepath)
-            dataArray = np.load(filepath, allow_pickle=True)
-            #print(dataArray['arr_0'])
-            np.savez_compressed(f, dataArray['arr_0'], allow_pickle=True)
-
-
-
-
-
-
-def extract_all(path: str, store_path: str):
-    var_list = get_var_name_list(path)
-    if not re.findall(r"/$", store_path):
-        store_path + "/"
-    if not os.path.exists(store_path):
-        os.mkdir(store_path)
-    date = get_date_range(path)
-    with Ds(path, 'r') as d:
-        for var_name in var_list:
-            var = np.asarray(d.variables[var_name])
-            file_name = os.path.realpath(store_path) + '/' + date + "_"     \
-                    + var_name + ".npz"
-            with open(file_name, 'wb') as f:
-                np.savez_compressed(f, var, allow_pickle=True)
-
-
-def create_metadata_file(data: str, dest: str, var_list: str):
-    pass
-
-
-def get_var_name_list(path: str) -> [str]:
-    """
-    List all variables in the specified file.
-    """
-    list = []
-    with Dataset(path, 'r') as d:
-        for var in d.variables.keys():
-            list.append(var)
-    return list
 
 def get_var_name_list(path: str) -> [str]:
     """
@@ -130,39 +208,3 @@ def get_var_name_list(path: str) -> [str]:
             list.append(var + "\t" + d.variables[var].units + ", " + 
                     d.variables[var].long_name)
     return list
-
-
-def print_all_vars(path: str):
-    for var in get_var_name_list(path):
-        print(var)
-
-
-
-def get_date_range(path: str) -> str:
-    with Ds(path, 'r') as d:
-        return str(d.RangeBeginningDate) + "_" + str(d.RangeEndingDate)
-
-
-
-#if __name__ == '__main__':
-#    fire.Fire({
-#        "single": extract_var_from_file,
-#        "all": extract_all
-#    })
-
-if __name__ == '__main__':
-    #check_src_path('../var_extract')
-    #check_dest_path('lol')
-    #check_dest_path('hi')
-    fire.Fire({
-        "list": print_all_vars
-    })
-
-    #concat('_PS', './test/', './test/PS.npz')
-    #concat('_H', './test/', './test/H.npz')
-    #concat('_O3', './test/', './test/O3.npz')
-    #concat('_EPV', './test/', './test/EPV.npz')
-
-#!/bin/env python
-
-
---
+++ programs/heat_map/heatmap.py
@@ -0,0 +1,144 @@
+#!/bin/env python
+
+import os
+import re
+import sys
+import fire
+import numpy as np
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
+file_dpi = 500
+
+def graph_var(src_file: str, dest_file:str):
+    with open(dest_file+".png", 'wb') as f:
+        src = np.load(src_file, allow_pickle=True)
+        print(src['arr_0'].shape)
+        data = src['arr_0'][12,:,:]
+
+        v_max = data[:,:].max()
+        v_min = data[:,:].min()
+
+        lats = np.load("lat.npz", allow_pickle=True)['arr_0']
+        lons = np.load("lon.npz", allow_pickle=True)['arr_0']
+
+        # Set the figure size, projection, and extent
+        fig = plt.figure(figsize=(10, 6))
+
+        emin = 65
+        emax = 83
+        nmin = 34
+        nmax = 48
+
+        ax = plt.axes(projection=ccrs.PlateCarree())
+        ax.set_global()
+        ax.set_extent([emin, emax, nmin, nmax], crs=ccrs.PlateCarree())   
+        ax.add_feature(cfeature.BORDERS, linewidth=1.4)
+        ax.gridlines(linestyle='--', color='black', draw_labels=True,
+                     linewidth=0.5)
+
+        res = int((v_max-v_min) / 20)
+
+        # Set contour levels, then draw the plot and a colorbar
+        clevs = np.arange(v_min, v_max, res)
+        plt.contourf(lons, lats, data, clevs, transform=ccrs.PlateCarree(),
+                     cmap=plt.cm.jet)
+
+        cb = plt.colorbar(ax=ax, orientation="vertical", pad=0.02, aspect=16,
+                          shrink=0.8)
+
+        cb.ax.tick_params(labelsize=10)
+
+        plt.title("Surface Pressure", size=14)
+        cb.set_label('Pa', size=12, rotation=0, labelpad=35)
+
+        fig.savefig(f, format='png', dpi=file_dpi)
+
+        
+if __name__ == '__main__':
+    fire.Fire({
+        "graph": graph_var
+    })
+
+
+def create_graph(var: str, name: str):
+    with Dataset(my_example_nc_file, mode='r') as data:
+        lons: [float] = data.variables['lon'][:]
+        lats: [float] = data.variables['lat'][:]
+
+        min, max = find_range(var)
+
+        d = data.variables[var][:, :, :]
+
+        d: [float, float, float] = d[0, :, :]
+
+        v_max = d[:].max()
+        v_min = var[:].min()
+
+        # Set the figure size, projection, and extent
+        fig = plt.figure(figsize=(10, 6))
+
+        ax = plt.axes(projection=ccrs.PlateCarree())
+        ax.set_global()
+        ax.set_extent([65, 83, 34, 48], crs=ccrs.PlateCarree())
+        ax.add_feature(cfeature.BORDERS, linewidth=1.4)
+        ax.gridlines(linestyle='--', color='black', draw_labels=True,
+                     linewidth=0.5)
+
+        res = int((max - min) / 20)
+
+        # Set contour levels, then draw the plot and a colorbar
+        clevs = np.arange(min, max, res)
+        plt.contourf(lons, lats, d, clevs, transform=ccrs.PlateCarree(),
+                     cmap=plt.cm.jet)
+
+        cb = plt.colorbar(ax=ax, orientation="vertical", pad=0.02, aspect=16,
+                          shrink=0.8)
+
+        cb.ax.tick_params(labelsize=10)
+        # TODO: make this more intelligent -- get time and date from file
+        if name == 'ps':
+            plt.title('Surface Pressure at 12am 01.07.2020', size=14)
+            cb.set_label('Pa', size=12, rotation=0, labelpad=15)
+        elif name == 'phis':
+            plt.title('Surface Geopotential Height at 12am 01.07.2020', size=14)
+            cb.set_label('m^2/s^-2', size=12, rotation=0, labelpad=15)
+
+        # Save the plot as a PNG image
+        fig.savefig('nc_plot_' + name + '.png', format=file_format,
+                    dpi=file_dpi)
+
+
+def list_all_var_data():
+    with Dataset(my_example_nc_file, mode='r') as data:
+        print(data.variables)
+
+
+def get_var_name_list() -> [str]:
+    list = []
+    with Dataset(my_example_nc_file, mode='r') as data:
+        for var in data.variables.keys():
+            list.append(var)
+    return list
+
+
+def list_var_names():
+    for var in get_var_name_list():
+        print(var)
+
+
+def extract_vars_into_files(list: [str], filepath: str):
+    for var in list:
+        os.system("ncks -v " +
+                  var + " " +
+                  filepath + "20200701.nc4 " +
+                  filepath + "20200701_" + var + ".nc4")
+
+
+def find_range(var_name):
+    with Dataset(my_example_nc_file, mode='r') as data:
+        var = data.variables[var_name]
+        max = var[:].max()
+        min = var[:].min()
+    return min, max
