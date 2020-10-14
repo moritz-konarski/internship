1775d206d64dd5aa63601fe01ca25d1984d4165c
Moritz Konarski <moritz.konarski@gmail.com>
2020-10-14 19:15:51 +0600

    work on data saving and plotting

--- programs/data_preparation/data_prep.py
+++ programs/data_preparation/data_prep.py
@@ -10,6 +10,11 @@ from pathlib import Path
 from netCDF4 import Dataset
 
 
+def var_name_format(name: str) -> str:
+    n = re.sub("_", " ", name)
+    return " ".join(w.capitalize() for w in n.split())
+
+
 def extract(src_path: str, dest_path: str, var_name: str):
     if not re.findall(r"/$", dest_path):
         dest_path += "/" + var_name + "/"
@@ -31,7 +36,11 @@ def extract(src_path: str, dest_path: str, var_name: str):
 
 def extract_and_save_data(file_list: [str], dest_path: str, var_name: str):
     n_files = len(file_list)
-    data = time = lat = lon = None
+    data = None
+    time = None
+    lat = None
+    lon = None
+    lev = None
     for (i, part) in enumerate(file_list):
         filepath = os.path.join(part)
         print(str(i+1) + "/" + str(n_files))
@@ -47,9 +56,10 @@ def extract_and_save_data(file_list: [str], dest_path: str, var_name: str):
         time = np.asarray(d.variables['time'])
         lat = np.asarray(d.variables['lat'])
         lon = np.asarray(d.variables['lon'])
+        lev = np.asarray(d.variables['lev'])
     print("Writing to file...")
     with open(dest_path, 'wb') as f:
-        np.savez_compressed(f, data=data, time=time, lat=lat, lon=lon, 
+        np.savez_compressed(f, data=data, time=time, lat=lat, lon=lon, lev=lev, 
             allow_pickle=True)
     
 def extract_metadata(dest_file: str, first_file: str, last_file: str,
@@ -57,12 +67,16 @@ def extract_metadata(dest_file: str, first_file: str, last_file: str,
     print("Extracting metadata...")
     name = long_name = std_name = units = shape = time_steps = None
     begin_date = end_date = lat_min = lat_max = lon_min = lon_max = None
-    data_min = data_max = None
+    lev_units = lat_units = lon_units = data_min = data_max = None 
+    lev_min = lev_max = None
     with Dataset(first_file, 'r') as d:
         name = d.variables[var].name
-        long_name = re.sub("_", " ", d.variables[var].long_name)
-        std_name =  re.sub("_", " ", d.variables[var].standard_name)
+        long_name = var_name_format(d.variables[var].long_name)
+        std_name =  var_name_format(d.variables[var].standard_name)
         units = d.variables[var].units
+        lat_units = d.variables['lat'].units
+        lon_units = d.variables['lon'].units
+        lev_units = d.variables['lev'].units
         begin_date = d.RangeBeginningDate
     with Dataset(last_file, 'r') as d:
         end_date = d.RangeEndingDate
@@ -70,10 +84,12 @@ def extract_metadata(dest_file: str, first_file: str, last_file: str,
     d = np.load(data_file, allow_pickle=True)
     shape = d['data'].shape
     time_steps = int(d['time'].shape[0])
-    lat_min = float(d['lat'][0])
-    lat_max = float(d['lat'][-1])
-    lon_min = float(d['lon'][0])
-    lon_max = float(d['lon'][-1])
+    lat_min = float(d['lat'].min())
+    lat_max = float(d['lat'].max())
+    lon_min = float(d['lon'].min())
+    lon_max = float(d['lon'].max())
+    lev_min = float(d['lev'].min())
+    lev_max = float(d['lev'].max())
     if len(shape) == 4:
         data_max = float(d['data'][:,:,:,:].max())
         data_min = float(d['data'][:,:,:,:].min())
@@ -89,7 +105,6 @@ def extract_metadata(dest_file: str, first_file: str, last_file: str,
             "shape" : shape,
             "data_max": data_max,
             "data_min": data_min,
-            "level_count" : 0 if len(shape) != 4 else int(shape[1]),
             "values_per_day" : time_steps,
             "day_count" : int(shape[0] / time_steps),
             "begin_date" : begin_date,
@@ -98,9 +113,15 @@ def extract_metadata(dest_file: str, first_file: str, last_file: str,
             "lat_min" : lat_min,
             "lat_max" : lat_max,
             "lat_count" : int(shape[-2]),
+            "lat_units" : lat_units,
             "lon_min" : lon_min,
             "lon_max" : lon_max,
-            "lon_count" : int(shape[-1])
+            "lon_count" : int(shape[-1]),
+            "lon_units" : lon_units,
+            "lev_min" : lev_min,
+            "lev_max" : lev_max,
+            "lev_count" : 0 if len(shape) != 4 else int(shape[1]),
+            "lev_units" : lev_units,
         }
     with open(dest_file, 'w') as f:
         json.dump(info_dict, f)
--- programs/plotting/plotting.py
+++ programs/plotting/plotting.py
@@ -4,62 +4,128 @@ import os
 import re
 import sys
 import fire
+import json
+import pprint
+import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
-file_dpi = 500
 
-def graph_var(src_file: str, dest_file:str):
-    with open(dest_file+".png", 'wb') as f:
-        # TODO: load lat and lon data from one file
-        src = np.load(src_file, allow_pickle=True)
-        #print(src['arr_0'].shape)
-        data = src['arr_0'][12,:,:]
+file_dpi = 300
 
-        v_max = data[:,:].max()
-        v_min = data[:,:].min()
 
-        lats = np.load("lat.npz", allow_pickle=True)['arr_0']
-        lons = np.load("lon.npz", allow_pickle=True)['arr_0']
+def get_data_info(src_folder: str):
+    with open(src_folder + "metadata.json", 'r') as f:
+        info_dict = json.load(f)
+    pprint.PrettyPrinter(indent=4, sort_dicts=False).pprint(info_dict)
 
-        # Set the figure size, projection, and extent
-        fig = plt.figure(figsize=(10, 6))
 
-        emin = 65
-        emax = 83
-        nmin = 34
-        nmax = 48
+def get_time_index(src_folder: str, graph_datetime: str, meta_dict) -> (int,
+        datetime):
+    start_datetime = \
+        datetime.datetime.strptime(meta_dict['begin_date'], '%Y-%m-%d')
+    end_datetime = \
+        datetime.datetime.strptime(graph_datetime, '%Y-%m-%d %H')
+    datetime_delta = end_datetime - start_datetime
+    return (int(datetime_delta.days * meta_dict['values_per_day'] 
+        + datetime_delta.seconds / 3600 / (24 / meta_dict['values_per_day'])),
+        end_datetime)
 
-        ax = plt.axes(projection=ccrs.PlateCarree())
-        ax.set_global()
-        ax.set_extent([emin, emax, nmin, nmax], crs=ccrs.PlateCarree())   
-        ax.add_feature(cfeature.BORDERS, linewidth=1.4)
-        ax.gridlines(linestyle='--', color='black', draw_labels=True,
-                     linewidth=0.5)
 
-        res = int((v_max-v_min) / 20)
+def heatmap(src_folder: str, name: str, graph_datetime: str, level: int):
+    meta_dict = None
+    with open(src_folder + "metadata.json", 'r') as f:
+        meta_dict = json.load(f)
+    src_file = src_folder + meta_dict['name'] + ".npz"
+    src = np.load(src_file, allow_pickle=True)
+    
+    time_index, graph_datetime = get_time_index(src_folder, graph_datetime, \
+            meta_dict)
 
-        # Set contour levels, then draw the plot and a colorbar
-        clevs = np.arange(v_min, v_max, res)
-        plt.contourf(lons, lats, data, clevs, transform=ccrs.PlateCarree(),
-                     cmap=plt.cm.jet)
+    if time_index < 0:
+        print("Date too small, minimum is: " + str(meta_dict['begin_date'])
+                + " 0")
+        exit(-1)
+    elif time_index > int(meta_dict['shape'][0])-1 and \
+            meta_dict['last_day_inclusive']:
+        print("Date too large, maximum is: " + str(meta_dict['end_date'])
+                + " 21")
+        exit(-1)
+    elif time_index > int(meta_dict['shape'][0])-1:
+        print("Date too large, maximum is (last day is not included): " \
+                + str(meta_dict['end_date']) + " 21")
+        exit(-1)
 
-        cb = plt.colorbar(ax=ax, orientation="vertical", pad=0.02, aspect=16,
-                          shrink=0.8)
+    print("Plotting...")
+    if meta_dict['lev_count'] == 0:
+        data = src['data'][time_index,:,:]
+        print(data.shape)
+    elif level < meta_dict['lev_count']:
+        data = src['data'][time_index, level,:,:]
+        print(data.shape)
+    else:
+        print("Level must be less than: " + str(meta_dict['lev_count']))
+        exit(-1)
 
-        cb.ax.tick_params(labelsize=10)
+    data_min = data[:,:].min()
+    print(data_min)
+    print(data_min.shape)
+    data_max = data[:,:].max()
+    print(data_max)
+    print(data_max.shape)
 
-        plt.title("Surface Pressure", size=14)
-        cb.set_label('Pa', size=12, rotation=0, labelpad=35)
+    lats = src['lat']
+    lons = src['lon']
 
+    # Set the figure size, projection, and extent
+    fig = plt.figure(figsize=(10, 6))
+
+    emin = meta_dict['lon_min']
+    emax = meta_dict['lon_max']
+    nmin = meta_dict['lat_min']
+    nmax = meta_dict['lat_max']
+
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.set_global()
+    ax.set_extent([emin, emax, nmin, nmax], crs=ccrs.PlateCarree())   
+    ax.add_feature(cfeature.BORDERS, linewidth=1.4)
+    ax.gridlines(linestyle='--', color='black', draw_labels=True,
+                 linewidth=0.5)
+
+    res = float((data_max - data_min) / 20)
+
+    # Set contour levels, then draw the plot and a colorbar
+    clevs = np.arange(data_min, data_max+res, res, dtype=float)
+    plt.contourf(lons, lats, data, clevs, transform=ccrs.PlateCarree(),
+                 cmap=plt.cm.jet)
+
+    cb = plt.colorbar(ax=ax, orientation="vertical", pad=0.02, aspect=16,
+                      shrink=0.8)
+
+    cb.ax.tick_params(labelsize=10)
+
+    title = "Heat Map of " + meta_dict['long_name'] + " on " \
+        + str(graph_datetime)
+
+    plt.title(title, size=14)
+    cb.set_label(meta_dict['units'], size=12, rotation=0, labelpad=35)
+
+    with open(name+".png", 'wb') as f:
         fig.savefig(f, format='png', dpi=file_dpi)
 
+def timeseries(src_folder: str, lat: float, lon: float):
+    pass
+
+def graph_var(src_file: str, dest_file:str):
+    pass
         
 if __name__ == '__main__':
     fire.Fire({
-        "graph": graph_var
+        "graph": graph_var,
+        "info": get_data_info,
+        "heatmap": heatmap
     })
 
 
@@ -158,11 +224,6 @@ def graph_var(src_file: str, dest_file:str):
         plt.title("Surface Pressure Time Series", size=14)
 
         fig.savefig(f, format='png', dpi=file_dpi)
-
-if __name__ == '__main__':
-    fire.Fire({
-        "graph": graph_var
-    })
 
 
 def create_graph(var: str, name: str):
