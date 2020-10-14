15b8fb351100eb37637c916731b665c7e99a342e
Moritz Konarski <moritz.konarski@gmail.com>
2020-10-14 16:20:52 +0600

    fix bugs in data preparation, remove files from plotting

--- programs/data_preparation/data_prep.py
+++ programs/data_preparation/data_prep.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from netCDF4 import Dataset
 
 
-def extract_var(src_path: str, dest_path: str, var_name: str):
+def extract(src_path: str, dest_path: str, var_name: str):
     if not re.findall(r"/$", dest_path):
         dest_path += "/" + var_name + "/"
     else:
@@ -22,84 +22,61 @@ def extract_var(src_path: str, dest_path: str, var_name: str):
     first_file = sorted_file_list[0]
     last_file = sorted_file_list[-1]
 
-    n_files = len(sorted_file_list)
-    # get data from files
-    with open(dest_path + var_name + ".npz", 'wb') as f:
-        data = None
-        for (i, part) in enumerate(sorted_file_list):
-            filepath = os.path.join(part)
-            print(str(i+1) + "/" + str(n_files))
-            with Dataset(filepath, 'r') as d:
-                if data is None:
-                    data = np.asarray(d.variables[var_name])
-                else:
-                    data = np.append(data, np.asarray(d.variables[var_name]), 
-                            axis=0)
-        time = None
-        lats = None
-        lons = None
-        part = sorted_file_list[0]
+    data_file = dest_path + var_name + ".npz"
+    extract_and_save_data(sorted_file_list, data_file, var_name)
+
+    dest_file = dest_path + "metadata.json"
+    extract_metadata(dest_file, first_file, last_file, data_file, var_name)
+
+# shape should be of the whole file
+
+def extract_and_save_data(file_list: [str], dest_path: str, var_name: str):
+    n_files = len(file_list)
+    data = time = lat = lon = None
+    for (i, part) in enumerate(file_list):
         filepath = os.path.join(part)
+        print(str(i+1) + "/" + str(n_files))
         with Dataset(filepath, 'r') as d:
-            time = np.asarray(d.variables['time'])
-            lats = np.asarray(d.variables['lat'])
-            lons = np.asarray(d.variables['lon'])
-        print("Writing to file...")
-        np.savez_compressed(f, data=data, time=time, lat=lats, lon=lons, 
+            if data is None:
+                data = np.asarray(d.variables[var_name])
+            else:
+                data = np.append(data, np.asarray(d.variables[var_name]), 
+                        axis=0)
+    #time = None
+    #lats = None
+    #lons = None
+    first_file = file_list[0]
+    filepath = os.path.join(first_file)
+    with Dataset(filepath, 'r') as d:
+        time = np.asarray(d.variables['time'])
+        lat = np.asarray(d.variables['lat'])
+        lon = np.asarray(d.variables['lon'])
+    print("Writing to file...")
+    with open(dest_path, 'wb') as f:
+        np.savez_compressed(f, data=data, time=time, lat=lat, lon=lon, 
             allow_pickle=True)
     
-    with open(dest_path + "metadata.json", 'w') as f:
-        json.dump(extract_metadata(first_file, last_file, var_name, n_files), f)
-
-
-def get_var_info_list(path: str) -> [str, str, str]:
-    """
-    List all variables, their names, units in the specified file.
-    """
-    list = []
-    with Dataset(path, 'r') as d:
-        for var in d.variables.keys():
-            list.append((var, d.variables[var].long_name, 
-                    d.variables[var].units))#, d.variables[var].dimension))
-        print(d.variables)
-    return list
-
-
-def print_all_vars(path: str):
-    for (var, name, unit) in get_var_info_list(path):
-        print("{0:6s} {1:15s} {2:s}".format(var, unit, name))
-
-
-# add count of the days
-def extract_metadata(first_file: str, last_file: str, var: str, count: int) -> dict:
-    name = None
-    long_name = None
-    std_name = None
-    units = None
-    shape = None
-    time_shape = None
-    begin_date = None
-    end_date = None
-    lat_min = None
-    lat_max = None
-    lon_min = None
-    lon_max = None
+def extract_metadata(dest_file: str, first_file: str, last_file: str,
+        data_file: str, var: str):
+    name = long_name = std_name = units = shape = time_steps = None
+    begin_date = end_date = lat_min = lat_max = lon_min = lon_max = None
     with Dataset(first_file, 'r') as d:
-        lat_min = float(d.variables['lat'][0])
-        lat_max = float(d.variables['lat'][-1])
-        lat_count = len(d.variables['lat'])
-        lon_min = float(d.variables['lon'][0])
-        lon_max = float(d.variables['lon'][-1])
-        lon_count = len(d.variables['lon'])
         name = d.variables[var].name
         long_name = re.sub("_", " ", d.variables[var].long_name)
         std_name =  re.sub("_", " ", d.variables[var].standard_name)
         units = d.variables[var].units
-        shape = d.variables[var].shape
-        time_shape = d.variables['time'].shape
         begin_date = d.RangeBeginningDate
     with Dataset(last_file, 'r') as d:
         end_date = d.RangeEndingDate
+
+    d = np.load(data_file, allow_pickle=True)
+    shape = d['data'].shape
+    time_steps = int(d['time'].shape[0])
+    lat_min = float(d['lat'][0])
+    lat_max = float(d['lat'][-1])
+    lon_min = float(d['lon'][0])
+    lon_max = float(d['lon'][-1])
+
     info_dict = {
             "name" : name,
             "long_name" : long_name,
@@ -107,22 +84,34 @@ def extract_metadata(first_file: str, last_file: str, var: str, count: int) -> d
             "units" : units,
             "shape" : shape,
             "level_count" : 0 if len(shape) != 4 else int(shape[1]),
-            "values_per_day" : int(time_shape[0]),
-            "day_count" : count,
+            "values_per_day" : time_steps,
+            "day_count" : int(shape[0] / time_steps),
             "begin_date" : begin_date,
             "end_date" : end_date,
+            "last_day_inclusive" : True,
             "lat_min" : lat_min,
             "lat_max" : lat_max,
-            "lat_count" : lat_count,
+            "lat_count" : int(shape[-2]),
             "lon_min" : lon_min,
             "lon_max" : lon_max,
-            "lon_count" : lon_count,
+            "lon_count" : int(shape[-1])
         }
-    return info_dict
+    with open(dest_file, 'w') as f:
+        json.dump(info_dict, f)
+
+
+def print_all_vars(path: str):
+    l = []
+    with Dataset(path, 'r') as d:
+        for var in d.variables.keys():
+            l.append((var, d.variables[var].long_name, 
+                    d.variables[var].units))
+    for (var, name, unit) in l:
+        print("{0:6s} {1:15s} {2:s}".format(var, unit, name))
 
 
 if __name__ == '__main__':
     fire.Fire({
         "list": print_all_vars,
-        "extract": extract_var
+        "extract": extract
     })
--- programs/plotting/heatmap.py
+++ programs/plotting/heatmap.py
@@ -143,3 +143,101 @@ def find_range(var_name):
         max = var[:].max()
         min = var[:].min()
     return min, max
+
+file_dpi = 500
+
+def graph_var(src_file: str, dest_file:str):
+    with open(dest_file+".png", 'wb') as f:
+        # TODO: have if statement for 
+        src = np.load(src_file, allow_pickle=True)
+        data = src['data'][:,10,10]
+
+        fig = plt.figure(figsize=(10, 6))
+        
+        plt.plot(data)
+        plt.title("Surface Pressure Time Series", size=14)
+
+        fig.savefig(f, format='png', dpi=file_dpi)
+
+if __name__ == '__main__':
+    fire.Fire({
+        "graph": graph_var
+    })
+
+
+def create_graph(var: str, name: str):
+    with Dataset(my_example_nc_file, mode='r') as data:
+        lons: [float] = data.variables['lon'][:]
+        lats: [float] = data.variables['lat'][:]
+
+        min, max = find_range(var)
+
+        d = data.variables[var][:, :, :]
+        d: [float, float, float] = d[0, :, :]
+
+        # Set the figure size, projection, and extent
+        fig = plt.figure(figsize=(10, 6))
+
+        ax = plt.axes(projection=ccrs.PlateCarree())
+        ax.set_global()
+        ax.set_extent([65, 83, 34, 48], crs=ccrs.PlateCarree())
+        ax.add_feature(cfeature.BORDERS, linewidth=1.4)
+        ax.gridlines(linestyle='--', color='black', draw_labels=True,
+                     linewidth=0.5)
+
+        res = int((max - min) / 20)
+
+        # Set contour levels, then draw the plot and a colorbar
+        clevs = np.arange(min, max, res)
+        plt.contourf(lons, lats, d, clevs, transform=ccrs.PlateCarree(),
+                     cmap=plt.cm.jet)
+
+        cb = plt.colorbar(ax=ax, orientation="vertical", pad=0.02, aspect=16,
+                          shrink=0.8)
+
+        cb.ax.tick_params(labelsize=10)
+        # TODO: make this more intelligent -- get time and date from file
+        if name == 'ps':
+            plt.title('Surface Pressure at 12am 01.07.2020', size=14)
+            cb.set_label('Pa', size=12, rotation=0, labelpad=15)
+        elif name == 'phis':
+            plt.title('Surface Geopotential Height at 12am 01.07.2020', size=14)
+            cb.set_label('m^2/s^-2', size=12, rotation=0, labelpad=15)
+
+        # Save the plot as a PNG image
+        fig.savefig('nc_plot_' + name + '.png', format=file_format,
+                    dpi=file_dpi)
+
+
+def list_all_var_data():
+    with Dataset(my_example_nc_file, mode='r') as data:
+        print(data.variables)
+
+
+def get_var_name_list() -> [str]:
+    list = []
+    with Dataset(my_example_nc_file, mode='r') as data:
+        for var in data.variables.keys():
+            list.append(var)
+    return list
+
+
+def list_var_names():
+    for var in get_var_name_list():
+        print(var)
+
+
+def extract_vars_into_files(list: [str], filepath: str):
+    for var in list:
+        os.system("ncks -v " +
+                  var + " " +
+                  filepath + "20200701.nc4 " +
+                  filepath + "20200701_" + var + ".nc4")
+
+
+def find_range(var_name):
+    with Dataset(my_example_nc_file, mode='r') as data:
+        var = data.variables[var_name]
+        max = var[:].max()
+        min = var[:].min()
+    return min, max
