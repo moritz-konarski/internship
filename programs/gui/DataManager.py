b89cb6b093b26a0b041bbc65da69e73f17a3b572
Moritz Konarski <moritz.konarski@gmail.com>
2020-11-04 02:06:07 +0600

    make simple data manager gui; crate popup for heat map data entry

--- programs/gui/DataManager.py
+++ programs/gui/DataManager.py
@@ -1,26 +1,114 @@
+import re
+import json
+import platform
+import datetime
+import numpy as np
+import pandas as pd
 from PlotObject import PlotType
 from PyQt5.QtCore import QThread, pyqtSlot, pyqtSignal
+from HelperFunctions import DirectorySeparator, FileExtension, HelperFunction
 
 
 class DataManager(QThread):
     def __init__(self, path: str):
+        super().__init__()
         self.thread_running = False
-        # here we initialize the data manager
-        self.metadata = ['die', 'there']
+
+        self.dir_separator = HelperFunction.get_dir_separator()
+
+        self.path = self.format_directory_path(path)
+
+        self.metadata_path = self.path + FileExtension.META_FILE.value
+
+        self.metadata_dictionary = None
+        try:
+            with open(self.metadata_path, 'r') as f:
+                self.metadata_dictionary = json.load(f)
+        except:
+            # TODO: exit out here
+            pass
+
+        self.var_name = self.metadata_dictionary['name']
+        self.data_path = self.path + self.var_name + FileExtension.DATA_FILE.value
+        self.begin_datetime = datetime.datetime.strptime(
+            self.metadata_dictionary['begin_date'] + " " + "0", "%Y-%m-%d %H")
+        self.end_datetime = datetime.datetime.strptime(
+            self.metadata_dictionary['end_date'] + " " + "21", "%Y-%m-%d %H")
+
+        self.level_count = self.metadata_dictionary['lev_count']
+        self.lat_min = self.metadata_dictionary['lat_min']
+        self.lat_max = self.metadata_dictionary['lat_max']
+        self.lon_min = self.metadata_dictionary['lon_min']
+        self.lon_max = self.metadata_dictionary['lon_max']
+
+    def get_data(self, start_time: datetime, end_time: datetime):
+        self.get_data(start_time, end_time, 0)
+
+    def get_data(self, start_time: datetime, end_time: datetime, level: int):
+        self.check_time_constraints(start_time, end_time)
+
+        if self.level_count == 0:
+            pass
+        else:
+            pass
+
+    def check_time_constraints(self, start_time: datetime, end_time: datetime) -> bool:
+        if start_time < self.begin_datetime:
+            # TODO: error out
+            pass
+        if end_time < self.end_datetime:
+            # TODO: error out
+            pass
+
+    def check_location_constraints(self, lat: float, lon: float) -> bool:
+        if lat < self.lat_min or lat > self.lat_max:
+            # TODO: error out
+            pass
+        if lon < self.lon_min or lon > self.lon_max:
+            # TODO: error out
+            pass
+
+    def check_level_constraints(self, level: int) -> bool:
+        if level < 0 or level > self.level_count:
+            # TODO: error out
+            pass
+
+    def get_time_series_data(self, start_time: datetime, end_time: datetime,
+                             level: int, lat: float, lon: float):
+        self.check_time_constraints(start_time, end_time)
+        self.check_location_constraints(lat, lon)
+        self.check_level_constraints(level)
+
+
         pass
+
+    def get_heat_map_data(self, start_time: datetime, end_time: datetime,
+                          level: int):
+        self.check_time_constraints(start_time, end_time)
+        self.check_level_constraints(level)
+
+        pass
+
+    def format_directory_path(self, path: str) -> str:
+        reg = r"{0}$".format(self.dir_separator)
+        if not re.findall(reg, path):
+            path += self.dir_separator
+        return path
 
     @pyqtSlot()
     def run(self):
         self.thread_running = True
-        pass
 
     def stop(self):
         self.thread_running = False
-        pass
 
     @property
     def metadata(self):
-        return self.metadata
+        return self.metadata_dictionary
+
+    @property
+    def shape(self) -> int:
+        return self.metadata_dictionary['shape']
 
     def get_plot_data(self, plot_type: PlotType):
         # return the appropriate type of slice
---
+++ programs/gui/HeatMapParameterWindow.py
@@ -0,0 +1,128 @@
+from PyQt5.QtWidgets import (QStatusBar, QWidget, QLabel, QPushButton,
+                             QProgressBar, QComboBox, QFileDialog, QMessageBox,
+                             QDateTimeEdit, QDateEdit, QDesktopWidget,
+                             QLineEdit)
+from PyQt5.QtCore import pyqtSlot
+from DataProcessor import DataProcessor
+from HelperFunctions import HelperFunction, ExportDataType
+from DataManager import DataManager
+from PyQt5 import QtCore
+import datetime
+
+
+class HeatMapParameterWindow(QWidget):
+    def __init__(self, parent, var_name: str, begin_date: datetime,
+                 end_date: datetime, level_count: int):
+        super().__init__()
+
+        self.title = 'Heat Map Parameters'
+        self.setWindowTitle(self.title)
+
+        self.margin = parent.margin
+        self.element_height = parent.element_height
+        self.button_width = parent.button_width
+        self.begin_date = begin_date
+        self.level = level_count
+        self.end_date = end_date
+        self.file_name = var_name
+        self.export_data_type = ExportDataType.CSV
+
+        text = "Choose Start Date and Time"
+        self.begin_date_edit_label = QLabel(self)
+        self.begin_date_edit_label.setText(text)
+        self.begin_date_edit_label \
+            .setGeometry(self.margin,
+                         10,
+                         HelperFunction.get_qt_text_width(
+                             self.begin_date_edit_label,
+                             text),
+                         self.element_height)
+        self.begin_date_edit = QDateTimeEdit(self, calendarPopup=True)
+        self.begin_date_edit.setDateTime(self.begin_date)
+        self.begin_date_edit.setGeometry(self.margin, 10 + self.element_height,
+                                         200, self.element_height)
+
+        text = "Choose End Date and Time"
+        self.end_date_edit_label = QLabel(self)
+        self.end_date_edit_label.setText(text)
+        self.end_date_edit_label \
+            .setGeometry(self.margin,
+                         10 + 2 * self.element_height,
+                         HelperFunction.get_qt_text_width(
+                             self.end_date_edit_label,
+                             text),
+                         self.element_height)
+        self.end_date_edit = QDateTimeEdit(self, calendarPopup=True)
+        self.end_date_edit.setDateTime(self.end_date)
+        self.end_date_edit.setGeometry(self.margin,
+                                       10 + 3 * self.element_height, 200,
+                                       self.element_height)
+
+        text = "Data Formats for Exporting"
+        self.export_data_type_combobox_label = QLabel(self)
+        self.export_data_type_combobox_label.setText(text)
+        self.export_data_type_combobox_label \
+            .setGeometry(self.margin,
+                         10 + 4 * self.element_height,
+                         HelperFunction.get_qt_text_width(
+                             self.export_data_type_combobox_label,
+                             text),
+                         self.element_height)
+
+        self.export_data_type_combobox = QComboBox(self)
+        self.export_data_type_combobox.addItems(
+            [part.value for part in ExportDataType])
+        self.export_data_type_combobox.setGeometry(self.margin,
+                                                   10 + 5 * self.element_height,
+                                                   self.button_width,
+                                                   self.element_height)
+
+        self.level_text_box = None
+        self.level_text_box_label = None
+        if level_count != 0:
+            text = "Choose Level to Export"
+            self.level_text_box_label = QLabel(self)
+            self.level_text_box_label.setText(text)
+            self.level_text_box_label \
+                .setGeometry(self.margin,
+                             10 + 6 * self.element_height,
+                             HelperFunction.get_qt_text_width(
+                                 self.level_text_box_label,
+                                 text),
+                             self.element_height)
+
+            self.level_text_box = QLineEdit(self)
+            self.level_text_box.setText(str(level_count))
+            self.level_text_box.setGeometry(self.margin,
+                                            10 + 7 * self.element_height,
+                                            self.button_width,
+                                            self.element_height)
+
+        text = "Set Parameters"
+        self.submit_button = QPushButton(text, self)
+        self.submit_button.clicked.connect(
+            self.return_data)
+        self.submit_button.setGeometry(
+            self.margin, 10 + 8.5 * self.element_height, self.button_width,
+            self.element_height)
+
+        self.setGeometry(0, 0, self.button_width + 2 * self.margin, 10 + 10 * self.element_height)
+        self.center_window()
+        self.show()
+
+    def return_data(self):
+        self.export_data_type = self.export_data_type_combobox.currentText()
+        self.begin_date = self.begin_date_edit.dateTime()
+        self.end_date = self.end_date_edit.dateTime()
+        if isinstance(self.level_text_box, QLineEdit):
+            self.level = int(self.level_text_box.text())
+        else:
+            self.level = 0
+        self.hide()
+
+    def center_window(self):
+        frame_geometry = self.frameGeometry()
+        center_point = QDesktopWidget().availableGeometry().center()
+        frame_geometry.moveCenter(center_point)
+        self.move(frame_geometry.topLeft())
+
